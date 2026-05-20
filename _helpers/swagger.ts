483697e3b98057be8271a89a62ea04944138b4df
John Michael P. Natingor <michaelnatingor@gmail.com>
2026-05-20 12:50:04 +0800

    Fix swagger.yaml path

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -4,6 +4,6 @@ import yaml from 'yamljs';
 import path from 'path';
 
 export function swaggerDocs(app: express.Application) {
-    const swaggerDocument = yaml.load(path.join(__dirname, '../swagger.yaml'));
+    const swaggerDocument = yaml.load(path.join(__dirname, '../../swagger.yaml'));
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
