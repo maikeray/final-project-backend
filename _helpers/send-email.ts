4f5670997d0fcf02d6fd0e1b6e4f78c95a8e2d2a
John Michael P. Natingor <michaelnatingor@gmail.com>
2026-05-22 04:12:58 +0800

    Fix SMTP TLS settings

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -4,14 +4,17 @@ export async function sendEmail({ to, subject, html, from = process.env.EMAIL_FR
     { to: string, subject: string, html: string, from?: string }) {
     
     const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT || '465'),
-    secure: true,
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-    }
-});
+        host: process.env.SMTP_HOST,
+        port: parseInt(process.env.SMTP_PORT || '587'),
+        secure: false,
+        tls: {
+            rejectUnauthorized: false
+        },
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
     
     await transporter.sendMail({ from, to, subject, html });
 }
