27d74a5db312acc5322de84fc12d04eadfb2b9e1
John Michael P. Natingor <michaelnatingor@gmail.com>
2026-05-22 03:49:51 +0800

    Use secure SMTP port 465

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -4,13 +4,14 @@ export async function sendEmail({ to, subject, html, from = process.env.EMAIL_FR
     { to: string, subject: string, html: string, from?: string }) {
     
     const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: parseInt(process.env.SMTP_PORT || '587'),
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
-        }
-    });
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT || '465'),
+    secure: true,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+});
     
     await transporter.sendMail({ from, to, subject, html });
 }
